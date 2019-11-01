f4d2f5ad2fdc883f31f96820719ca879e963f467
Van Nguyen <1751012082van@ou.edu.vn>
2019-11-01 16:24:42 +0700

    Add new Frm_ForgotPassword

--- Frm_ForgotPassword.Designer.cs
+++ Frm_ForgotPassword.Designer.cs
@@ -29,11 +29,202 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_ForgotPassword));
+			this.btnSignUp = new System.Windows.Forms.Button();
+			this.btnLogIn = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnDoipw = new System.Windows.Forms.Button();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.txtCNpw = new System.Windows.Forms.TextBox();
+			this.txtNpw = new System.Windows.Forms.TextBox();
+			this.txtUser = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnSignUp
+			// 
+			this.btnSignUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSignUp.ForeColor = System.Drawing.Color.DarkGray;
+			this.btnSignUp.Location = new System.Drawing.Point(66, 438);
+			this.btnSignUp.Name = "btnSignUp";
+			this.btnSignUp.Size = new System.Drawing.Size(106, 36);
+			this.btnSignUp.TabIndex = 28;
+			this.btnSignUp.Text = "Sign Up";
+			this.btnSignUp.UseVisualStyleBackColor = true;
+			// 
+			// btnLogIn
+			// 
+			this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLogIn.ForeColor = System.Drawing.Color.DarkGray;
+			this.btnLogIn.Location = new System.Drawing.Point(537, 438);
+			this.btnLogIn.Name = "btnLogIn";
+			this.btnLogIn.Size = new System.Drawing.Size(106, 36);
+			this.btnLogIn.TabIndex = 27;
+			this.btnLogIn.Text = "Login";
+			this.btnLogIn.UseVisualStyleBackColor = true;
+			this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.GhostWhite;
+			this.panel1.Controls.Add(this.btnDoipw);
+			this.panel1.Controls.Add(this.checkBox3);
+			this.panel1.Controls.Add(this.checkBox2);
+			this.panel1.Controls.Add(this.txtCNpw);
+			this.panel1.Controls.Add(this.txtNpw);
+			this.panel1.Controls.Add(this.txtUser);
+			this.panel1.Location = new System.Drawing.Point(19, 191);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(670, 228);
+			this.panel1.TabIndex = 25;
+			// 
+			// btnDoipw
+			// 
+			this.btnDoipw.BackColor = System.Drawing.Color.LightSeaGreen;
+			this.btnDoipw.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnDoipw.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDoipw.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnDoipw.Location = new System.Drawing.Point(214, 172);
+			this.btnDoipw.Name = "btnDoipw";
+			this.btnDoipw.Size = new System.Drawing.Size(242, 44);
+			this.btnDoipw.TabIndex = 5;
+			this.btnDoipw.Text = "HELP ME";
+			this.btnDoipw.UseVisualStyleBackColor = false;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox3.Location = new System.Drawing.Point(527, 132);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(110, 22);
+			this.checkBox3.TabIndex = 22;
+			this.checkBox3.Text = "Hide / Show";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox2.Location = new System.Drawing.Point(527, 86);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(114, 22);
+			this.checkBox2.TabIndex = 21;
+			this.checkBox2.Text = "Hide / Show ";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// txtCNpw
+			// 
+			this.txtCNpw.BackColor = System.Drawing.Color.Silver;
+			this.txtCNpw.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtCNpw.ForeColor = System.Drawing.Color.White;
+			this.txtCNpw.Location = new System.Drawing.Point(191, 114);
+			this.txtCNpw.Multiline = true;
+			this.txtCNpw.Name = "txtCNpw";
+			this.txtCNpw.Size = new System.Drawing.Size(295, 40);
+			this.txtCNpw.TabIndex = 4;
+			this.txtCNpw.Text = "Confirm New Password";
+			// 
+			// txtNpw
+			// 
+			this.txtNpw.BackColor = System.Drawing.Color.Silver;
+			this.txtNpw.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtNpw.ForeColor = System.Drawing.Color.White;
+			this.txtNpw.Location = new System.Drawing.Point(191, 66);
+			this.txtNpw.Multiline = true;
+			this.txtNpw.Name = "txtNpw";
+			this.txtNpw.Size = new System.Drawing.Size(295, 42);
+			this.txtNpw.TabIndex = 3;
+			this.txtNpw.Text = "New Password";
+			// 
+			// txtUser
+			// 
+			this.txtUser.BackColor = System.Drawing.Color.Silver;
+			this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtUser.ForeColor = System.Drawing.Color.White;
+			this.txtUser.Location = new System.Drawing.Point(191, 16);
+			this.txtUser.Multiline = true;
+			this.txtUser.Name = "txtUser";
+			this.txtUser.Size = new System.Drawing.Size(295, 44);
+			this.txtUser.TabIndex = 2;
+			this.txtUser.Text = "User";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.LightSeaGreen;
+			this.label2.Location = new System.Drawing.Point(130, 152);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(455, 36);
+			this.label2.TabIndex = 26;
+			this.label2.Text = "FORGOT YOUR PASSWORD?";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(287, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 29;
+			this.pictureBox1.TabStop = false;
+			// 
+			// errorProvider1
+			// 
+			this.errorProvider1.ContainerControl = this;
+			// 
+			// errorProvider2
+			// 
+			this.errorProvider2.ContainerControl = this;
+			// 
+			// Frm_ForgotPassword
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(717, 492);
+			this.Controls.Add(this.btnSignUp);
+			this.Controls.Add(this.btnLogIn);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Frm_ForgotPassword";
 			this.Text = "Frm_ForgotPassword";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btnSignUp;
+		private System.Windows.Forms.Button btnLogIn;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnDoipw;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.TextBox txtCNpw;
+		private System.Windows.Forms.TextBox txtNpw;
+		private System.Windows.Forms.TextBox txtUser;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ErrorProvider errorProvider1;
+		private System.Windows.Forms.ErrorProvider errorProvider2;
 	}
 }
--- Frm_ForgotPassword.cs
+++ Frm_ForgotPassword.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Globalization;
+using System.Configuration;
 
 namespace QLBH_KiemThuPhanMem
 {
 	public partial class Frm_ForgotPassword : Form
 	{
+		SqlConnection sqlcon = new SqlConnection(@"Data Source=VAN;Initial Catalog=QLBH;Integrated Security=True");
 		public Frm_ForgotPassword()
 		{
 			InitializeComponent();
+		}
+		public void Forgot()
+		{
+
+			try
+			{
+				sqlcon.Open();
+				string id = txtUser.Text;
+				if (id != null) // Textbox không bỏ trống
+				{
+					string sql = "Select Count(*) From [QLNV].[dbo].[QLNV_Login] Where ID=@id ";
+					SqlCommand cmd = new SqlCommand(sql, sqlcon);
+					cmd.Parameters.Add(new SqlParameter("@id", id));
+					int x = (int)cmd.ExecuteScalar();
+					if (x == 1) // đúng id
+					{
+						string Newpw = txtNpw.Text.Trim();
+						string Cfpw = txtCNpw.Text.Trim();
+						int countSo = 0;
+						int countHoa = 0;
+						int countThuong = 0;
+						int countDB = 0;
+						int i = 0;
+						// Kiểm soát chiều dài PW
+						if (Newpw.Length < 6 || Newpw.Length > 8)
+						{
+							errorProvider1.SetError(txtNpw, "Password must be at least 6 character and no more than 8 !"); // [6,8]
+							txtNpw.Text = string.Empty;
+							txtCNpw.Text = string.Empty;
+							btnDoipw.Enabled = false; // vô hiệu hóa nút đổi Mật khẩu
+						}
+						else // chưa bắt được lỗi nhập Khoảng trắng đầu cuối
+						{
+							while (i < Newpw.Length)
+							{
+								if (Newpw[i] >= 'a' && Newpw[i] <= 'z')
+								{
+									countThuong++;
+								}
+								else if (Newpw[i] >= 'A' && Newpw[i] <= 'Z')
+								{
+									countHoa++;
+								}
+								else if (Newpw[i] >= '0' && Newpw[i] <= '9')
+								{
+									countSo++;
+								}
+								else
+								{
+									countDB++;
+								}
+								i++;
+							}
+							if (countThuong >= 1 && countHoa >= 1 && countSo >= 1 && countDB >= 1)
+							{
+								btnDoipw.Enabled = true;
+							}
+						}
+						// So sánh PW mới với xác nhận PW
+						if (String.Compare(Newpw, Cfpw, false) == 0)
+						{
+							btnDoipw.Enabled = true;
+							string sqlUpdatePW = "UPDATE [QLNV].[dbo].[QLNV_Login] SET Password='" + txtCNpw.Text + "' WHERE ID='" + txtUser.Text + "'";
+							SqlCommand cmdUpDatePW = new SqlCommand(sqlUpdatePW, sqlcon);
+							SqlDataAdapter dap = new SqlDataAdapter(cmdUpDatePW);
+							DataTable dt = new DataTable();
+							dap.Fill(dt);
+							cmdUpDatePW.ExecuteNonQuery();
+							MessageBox.Show("Your password has been changed successfully !");
+							sqlcon.Close();
+						}
+						else
+						{
+							errorProvider1.SetError(txtCNpw, "You need to confirm excactly!");
+							Cfpw = string.Empty;
+						}
+					}
+					else
+					{
+						MessageBox.Show("User is not correct! Please try again ", "ERROR");
+					}
+				}
+				else
+				{
+					if (txtUser.Text == "")
+					{
+						errorProvider1.SetError(txtUser, "Do not accept blank fields!");
+					}
+				}
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Connection failed !", "Try Again");
+			}
+			finally
+			{
+				sqlcon.Close();
+			}
+		}
+
+		private void btnLogIn_Click(object sender, EventArgs e)
+		{
+			Frm_SignIn login = new Frm_SignIn();
+			login.Show();
+			this.Hide();
 		}
 	}
 }
