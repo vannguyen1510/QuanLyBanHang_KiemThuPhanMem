3e3105810fce451b1e65343bf906fe27e0655c43
Van Nguyen <1751012082van@ou.edu.vn>
2019-11-03 17:14:09 +0700

    Fix Frm_Main_Customers

--- Frm_Main_Customers.Designer.cs
+++ Frm_Main_Customers.Designer.cs
@@ -33,17 +33,32 @@
 			this.toolStrip_btnSunG = new System.Windows.Forms.ToolStripButton();
 			this.toolStrip_btnG = new System.Windows.Forms.ToolStripButton();
 			this.toolStrip_btnUs = new System.Windows.Forms.ToolStripButton();
-			this.pbBack_SunG = new System.Windows.Forms.PictureBox();
-			this.pbNext_SunG = new System.Windows.Forms.PictureBox();
 			this.flpSunG = new System.Windows.Forms.FlowLayoutPanel();
-			this.pbBack_G = new System.Windows.Forms.PictureBox();
-			this.flpG = new System.Windows.Forms.FlowLayoutPanel();
-			this.pbNext_G = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnShopNow_SunG_1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.btnBack_SunG = new System.Windows.Forms.Button();
+			this.btnNext_SunG = new System.Windows.Forms.Button();
 			this.toolStrip1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pbBack_SunG)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pbNext_SunG)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pbBack_G)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pbNext_G)).BeginInit();
+			this.flpSunG.SuspendLayout();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// toolStrip1
@@ -60,7 +75,7 @@
 			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
 			this.toolStrip1.Name = "toolStrip1";
 			this.toolStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-			this.toolStrip1.Size = new System.Drawing.Size(1118, 31);
+			this.toolStrip1.Size = new System.Drawing.Size(1483, 31);
 			this.toolStrip1.TabIndex = 0;
 			this.toolStrip1.Text = "toolStrip1";
 			// 
@@ -93,72 +108,215 @@
 			this.toolStrip_btnUs.Text = "ABOUT US";
 			this.toolStrip_btnUs.ToolTipText = "Nice too meet you!";
 			// 
-			// pbBack_SunG
-			// 
-			this.pbBack_SunG.Image = ((System.Drawing.Image)(resources.GetObject("pbBack_SunG.Image")));
-			this.pbBack_SunG.Location = new System.Drawing.Point(7, 169);
-			this.pbBack_SunG.Name = "pbBack_SunG";
-			this.pbBack_SunG.Size = new System.Drawing.Size(59, 91);
-			this.pbBack_SunG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pbBack_SunG.TabIndex = 2;
-			this.pbBack_SunG.TabStop = false;
-			// 
-			// pbNext_SunG
-			// 
-			this.pbNext_SunG.Image = ((System.Drawing.Image)(resources.GetObject("pbNext_SunG.Image")));
-			this.pbNext_SunG.Location = new System.Drawing.Point(1048, 169);
-			this.pbNext_SunG.Name = "pbNext_SunG";
-			this.pbNext_SunG.Size = new System.Drawing.Size(59, 91);
-			this.pbNext_SunG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pbNext_SunG.TabIndex = 3;
-			this.pbNext_SunG.TabStop = false;
-			// 
 			// flpSunG
 			// 
-			this.flpSunG.Location = new System.Drawing.Point(85, 107);
+			this.flpSunG.Controls.Add(this.panel1);
+			this.flpSunG.Controls.Add(this.panel2);
+			this.flpSunG.Controls.Add(this.panel3);
+			this.flpSunG.Location = new System.Drawing.Point(93, 110);
 			this.flpSunG.Name = "flpSunG";
-			this.flpSunG.Size = new System.Drawing.Size(947, 211);
-			this.flpSunG.TabIndex = 4;
+			this.flpSunG.Size = new System.Drawing.Size(950, 282);
+			this.flpSunG.TabIndex = 1;
+			this.flpSunG.TabStop = true;
 			// 
-			// pbBack_G
+			// panel1
 			// 
-			this.pbBack_G.Image = ((System.Drawing.Image)(resources.GetObject("pbBack_G.Image")));
-			this.pbBack_G.Location = new System.Drawing.Point(7, 483);
-			this.pbBack_G.Name = "pbBack_G";
-			this.pbBack_G.Size = new System.Drawing.Size(59, 91);
-			this.pbBack_G.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pbBack_G.TabIndex = 5;
-			this.pbBack_G.TabStop = false;
+			this.panel1.Controls.Add(this.btnShopNow_SunG_1);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Location = new System.Drawing.Point(3, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(940, 268);
+			this.panel1.TabIndex = 0;
 			// 
-			// flpG
+			// btnShopNow_SunG_1
 			// 
-			this.flpG.Location = new System.Drawing.Point(85, 417);
-			this.flpG.Name = "flpG";
-			this.flpG.Size = new System.Drawing.Size(947, 211);
-			this.flpG.TabIndex = 5;
+			this.btnShopNow_SunG_1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnShopNow_SunG_1.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnShopNow_SunG_1.Location = new System.Drawing.Point(559, 167);
+			this.btnShopNow_SunG_1.Name = "btnShopNow_SunG_1";
+			this.btnShopNow_SunG_1.Size = new System.Drawing.Size(220, 38);
+			this.btnShopNow_SunG_1.TabIndex = 3;
+			this.btnShopNow_SunG_1.Text = "SHOP NOW";
+			this.btnShopNow_SunG_1.UseVisualStyleBackColor = true;
 			// 
-			// pbNext_G
+			// label2
 			// 
-			this.pbNext_G.Image = ((System.Drawing.Image)(resources.GetObject("pbNext_G.Image")));
-			this.pbNext_G.Location = new System.Drawing.Point(1048, 483);
-			this.pbNext_G.Name = "pbNext_G";
-			this.pbNext_G.Size = new System.Drawing.Size(59, 91);
-			this.pbNext_G.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pbNext_G.TabIndex = 6;
-			this.pbNext_G.TabStop = false;
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(595, 52);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(141, 36);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "M.PRI$$ 01";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Franklin Gothic Book", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(461, 88);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(407, 74);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Sleek black acetate frame with metal snake details in gold color surrounding the " +
+    "front";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(68, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(294, 265);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.button1);
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Controls.Add(this.label4);
+			this.panel2.Controls.Add(this.pictureBox2);
+			this.panel2.Location = new System.Drawing.Point(3, 277);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(940, 268);
+			this.panel2.TabIndex = 4;
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(559, 167);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(220, 38);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "SHOP NOW";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(595, 52);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(141, 36);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "M.PRI$$ 01";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Franklin Gothic Book", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(461, 88);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(407, 74);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "Sleek black acetate frame with metal snake details in gold color surrounding the " +
+    "front";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(68, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(294, 265);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 0;
+			this.pictureBox2.TabStop = false;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.button2);
+			this.panel3.Controls.Add(this.label5);
+			this.panel3.Controls.Add(this.label6);
+			this.panel3.Controls.Add(this.pictureBox3);
+			this.panel3.Location = new System.Drawing.Point(3, 551);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(940, 268);
+			this.panel3.TabIndex = 5;
+			// 
+			// button2
+			// 
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button2.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(559, 167);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(220, 38);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "SHOP NOW";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(595, 52);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(141, 36);
+			this.label5.TabIndex = 2;
+			this.label5.Text = "M.PRI$$ 01";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Franklin Gothic Book", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(461, 88);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(407, 74);
+			this.label6.TabIndex = 1;
+			this.label6.Text = "Sleek black acetate frame with metal snake details in gold color surrounding the " +
+    "front";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(68, 0);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(294, 265);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 0;
+			this.pictureBox3.TabStop = false;
+			// 
+			// btnBack_SunG
+			// 
+			this.btnBack_SunG.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBack_SunG.BackgroundImage")));
+			this.btnBack_SunG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnBack_SunG.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnBack_SunG.Image = ((System.Drawing.Image)(resources.GetObject("btnBack_SunG.Image")));
+			this.btnBack_SunG.Location = new System.Drawing.Point(12, 208);
+			this.btnBack_SunG.Name = "btnBack_SunG";
+			this.btnBack_SunG.Size = new System.Drawing.Size(60, 100);
+			this.btnBack_SunG.TabIndex = 2;
+			this.btnBack_SunG.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnBack_SunG.UseVisualStyleBackColor = true;
+			this.btnBack_SunG.Click += new System.EventHandler(this.btnBack_SunG_Click);
+			// 
+			// btnNext_SunG
+			// 
+			this.btnNext_SunG.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNext_SunG.BackgroundImage")));
+			this.btnNext_SunG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnNext_SunG.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnNext_SunG.Location = new System.Drawing.Point(533, 513);
+			this.btnNext_SunG.Name = "btnNext_SunG";
+			this.btnNext_SunG.Size = new System.Drawing.Size(63, 100);
+			this.btnNext_SunG.TabIndex = 3;
+			this.btnNext_SunG.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnNext_SunG.UseVisualStyleBackColor = true;
+			this.btnNext_SunG.Click += new System.EventHandler(this.btnNext_SunG_Click);
 			// 
 			// Frm_Main_Customers
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.ClientSize = new System.Drawing.Size(1118, 705);
-			this.Controls.Add(this.pbNext_G);
-			this.Controls.Add(this.flpG);
-			this.Controls.Add(this.pbBack_G);
+			this.ClientSize = new System.Drawing.Size(1483, 681);
+			this.Controls.Add(this.btnNext_SunG);
+			this.Controls.Add(this.btnBack_SunG);
 			this.Controls.Add(this.flpSunG);
-			this.Controls.Add(this.pbNext_SunG);
-			this.Controls.Add(this.pbBack_SunG);
 			this.Controls.Add(this.toolStrip1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -166,12 +324,19 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "EYEWEAR - We are the opposite of mass production";
 			this.TopMost = true;
+			this.Load += new System.EventHandler(this.Frm_Main_Customers_Load);
 			this.toolStrip1.ResumeLayout(false);
 			this.toolStrip1.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pbBack_SunG)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pbNext_SunG)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pbBack_G)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pbNext_G)).EndInit();
+			this.flpSunG.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -183,11 +348,23 @@
 		private System.Windows.Forms.ToolStripButton toolStrip_btnSunG;
 		private System.Windows.Forms.ToolStripButton toolStrip_btnG;
 		private System.Windows.Forms.ToolStripButton toolStrip_btnUs;
-		private System.Windows.Forms.PictureBox pbBack_SunG;
-		private System.Windows.Forms.PictureBox pbNext_SunG;
 		private System.Windows.Forms.FlowLayoutPanel flpSunG;
-		private System.Windows.Forms.PictureBox pbBack_G;
-		private System.Windows.Forms.FlowLayoutPanel flpG;
-		private System.Windows.Forms.PictureBox pbNext_G;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnBack_SunG;
+		private System.Windows.Forms.Button btnNext_SunG;
+		private System.Windows.Forms.Button btnShopNow_SunG_1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.PictureBox pictureBox3;
 	}
 }
--- Frm_Main_Customers.cs
+++ Frm_Main_Customers.cs
@@ -15,8 +15,58 @@ namespace QLBH_KiemThuPhanMem
 		public Frm_Main_Customers()
 		{
 			InitializeComponent();
+			flpSunG.AutoScroll = false;
 		}
 
+		private void Frm_Main_Customers_Load(object sender, EventArgs e)
+		{
+
+		}
+		//public int scrollValue = 0;
+		//public int ScrollValue
+		//{
+		//	get
+		//	{
+		//		return scrollValue;
+		//	}
+		//	set 
+		//	{
+		//		scrollValue = value;
+		//		flpSunG.VerticalScroll.Maximum = 100;
+		//		if(scrollValue < flpSunG.VerticalScroll.Minimum)
+		//		{
+		//			scrollValue = flpSunG.VerticalScroll.Minimum;
+		//		}
+		//		if(scrollValue > flpSunG.VerticalScroll.Maximum)
+		//		{
+		//			scrollValue = flpSunG.VerticalScroll.Maximum;
+		//		}
+		//		flpSunG.VerticalScroll.Value = scrollValue;
+		//		flpSunG.PerformLayout();
+		//	}
+		//}
+
+		private const int VerticalStep = 40;
 		
+
+		private void btnNext_SunG_Click(object sender, EventArgs e)
+		{
+			//int change = flpSunG.VerticalScroll.Value + flpSunG.VerticalScroll.SmallChange * 30;
+			//flpSunG.AutoScrollPosition = new Point(0,change);
+
+			//scrollValue -= flpSunG.VerticalScroll.LargeChange;
+
+			flpSunG.Top -= VerticalStep;
+		}
+
+		private void btnBack_SunG_Click(object sender, EventArgs e)
+		{
+			//int change = flpSunG.VerticalScroll.Value - flpSunG.VerticalScroll.SmallChange * 30;
+			//flpSunG.AutoScrollPosition = new Point(0, change);
+
+			//scrollValue += flpSunG.VerticalScroll.LargeChange;
+
+			flpSunG.Top += VerticalStep;
+		}
 	}
 }
